8611aa024386368e6f7a5457f55314ce106cc908
Tim Siegel <siegeltr@gmail.com>
2022-10-01 00:30:07 -0400

    Use a floating-point font aspect ratio

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use image::{GenericImageView, Pixel};
 
+const FONT_RATIO: f32 = 6. / 13.;
+
 const fn get_ascii_pixel(intensity: u8) -> char {
     [' ', '.', ',', '-', '~', '+', '=', '@'][intensity as usize / 32]
 }
@@ -10,7 +12,7 @@ fn get_image(dir: &str, columns: u32) {
     let (width, height) = img.dimensions();
     let columns = if columns == 0 { 80 } else { columns.min(width) };
     let x_scale = width as f32 / columns as f32;
-    let y_scale = x_scale * 2.;
+    let y_scale = x_scale / FONT_RATIO;
     for y in 0.. {
         let yy = (y as f32 * y_scale) as u32;
         if yy >= height {
