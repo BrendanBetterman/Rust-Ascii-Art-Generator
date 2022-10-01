393eec25e04f795dfc7126c0863eed8c2ffc0674
Tim Siegel <siegeltr@gmail.com>
2022-09-30 22:17:33 -0400

    Normalize style using cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,32 @@
-use image::{GenericImageView};
+use image::GenericImageView;
 
-fn get_str_ascii(intent :u8)-> &'static str{
-    let index = intent/32;
-    let ascii = [" ",".",",","-","~","+","=","@"];
+fn get_str_ascii(intent: u8) -> &'static str {
+    let index = intent / 32;
+    let ascii = [" ", ".", ",", "-", "~", "+", "=", "@"];
     return ascii[index as usize];
 }
 
-fn get_image(dir: &str, scale: u32){
+fn get_image(dir: &str, scale: u32) {
     let img = image::open(dir).unwrap();
     println!("{:?}", img.dimensions());
-    let (width,height) = img.dimensions();
-    for y in 0..height{
-        for x in 0..width{
-            if y % (scale * 2) == 0 && x % scale ==0{
-                let pix = img.get_pixel(x,y);
-                let mut intent = pix[0]/3 + pix[1]/3 + pix[2]/3;
-                if pix[3] ==0{
+    let (width, height) = img.dimensions();
+    for y in 0..height {
+        for x in 0..width {
+            if y % (scale * 2) == 0 && x % scale == 0 {
+                let pix = img.get_pixel(x, y);
+                let mut intent = pix[0] / 3 + pix[1] / 3 + pix[2] / 3;
+                if pix[3] == 0 {
                     intent = 0;
                 }
-                print!("{}",get_str_ascii(intent));
-            } 
+                print!("{}", get_str_ascii(intent));
+            }
         }
-        if y%(scale*2)==0{
+        if y % (scale * 2) == 0 {
             println!("");
         }
     }
 }
+
 fn main() {
-    get_image("pug.png",4);
+    get_image("pug.png", 4);
 }
