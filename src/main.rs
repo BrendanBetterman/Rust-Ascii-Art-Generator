2e06e415c304738ac1b91df80aeb3693083f24df
Tim Siegel <siegeltr@gmail.com>
2022-09-30 23:29:46 -0400

    Scale image to column width, rather than a fixed ratio

--- src/main.rs
+++ src/main.rs
@@ -4,28 +4,37 @@ const fn get_ascii_pixel(intensity: u8) -> char {
     [' ', '.', ',', '-', '~', '+', '=', '@'][intensity as usize / 32]
 }
 
-fn get_image(dir: &str, scale: u32) {
+fn get_image(dir: &str, columns: u32) {
     let img = image::open(dir).unwrap();
     println!("{:?}", img.dimensions());
     let (width, height) = img.dimensions();
-    for y in 0..height {
-        for x in 0..width {
-            if y % (scale * 2) == 0 && x % scale == 0 {
-                let pix = img.get_pixel(x, y);
-                let intensity = if pix[3] == 0 {
-                    0
-                } else {
-                    pix.to_luma()[0]
-                };
-                print!("{}", get_ascii_pixel(intensity));
+    let x_scale = width as f32 / columns.min(width) as f32;
+    let y_scale = x_scale * 2.;
+    for y in 0.. {
+        let yy = (y as f32 * y_scale) as u32;
+        if yy >= height {
+            break;
+        }
+
+        for x in 0.. {
+            let xx = (x as f32 * x_scale) as u32;
+            if xx >= width {
+                break;
             }
+
+            let pix = img.get_pixel(xx, yy);
+            let intensity = if pix[3] == 0 {
+                0
+            } else {
+                pix.to_luma()[0]
+            };
+            print!("{}", get_ascii_pixel(intensity));
         }
-        if y % (scale * 2) == 0 {
-            println!("");
-        }
+
+        println!();
     }
 }
 
 fn main() {
-    get_image("assets/pug.png", 4);
+    get_image("assets/pug.png", 157);
 }
